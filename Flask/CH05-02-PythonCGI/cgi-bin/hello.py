b34254037a2258bc780df9fbc6e1052cae966de4
Joel Joy <zandy212@126.com>
2018-03-16 00:24:02 +0800

    2018/3/16

---
+++ Flask/CH05-02-PythonCGI/cgi-bin/hello.py
@@ -0,0 +1,18 @@
+# 声明网页输出
+print('Content-type: text/html\n')
+
+html = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>优品课堂 Python CGI</title>
+</head>
+<body>
+    <h1>优品课堂 Python CGI</h1>
+    <img src="http://temp.im/300x200" alt="">
+</body>
+</html>"""
+
+
+print(html)
--- Flask/CH05-02-PythonCGI/webserver.py
+++ Flask/CH05-02-PythonCGI/webserver.py
@@ -1,12 +1,14 @@
 import os
+# 操作系统相关
 from http.server import HTTPServer, CGIHTTPRequestHandler
+# CGI:通用网关接口
 
-webdir = '.'    # 当前目录
-port = 80
+webdir = '.'  # '.'代表当前目录
+port = 80     # 端口
 
 os.chdir(webdir)
 server_address = ('', port)
 server_obj = HTTPServer(server_address, CGIHTTPRequestHandler)
-server_obj.serve_forever()
+server_obj.serve_forever()   # 启动服务器
 
 
