78a03b902abc3c01d1ba7e410f509991d1975138
Joel Joy <zandy212@126.com>
2018-03-24 22:18:21 +0800

    2018/3/24

--- Flask/05-Request/rq.py
+++ Flask/05-Request/rq.py
@@ -5,16 +5,33 @@ from urllib.parse import unquote
 app = Flask(__name__)
 app.debug = True
 
-@app.route('/')
+@app.route('/', methods=['GET'])  
 def index():
 	return render_template('index.html')
+
+@app.route('/reg/')
+def reg():
+    return render_template('reg.html')
+
+@app.route('/headers/')
+def headers():
+    data = dict(request.headers)
+    return str(data)
+
 
 @app.route('/rq/')
 def get_request():
 	path = request.path
 	method = request.method
-	name = unquote(request.args.get('name','未找到'))
+	name = unquote(request.values['name'])
 	return name
+
+@app.route('/doreg/', methods=['GET','POST'])
+def do_reg():
+    name = request.values['username']
+    pwd = request.values['pwd']
+    # age = request.args.get('age', 0)
+    return '姓名:{},密码:{}'.format(name, pwd)
 
 if __name__ == '__main__':
 	app.run()
