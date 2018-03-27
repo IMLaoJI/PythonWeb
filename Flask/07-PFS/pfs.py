261bc2acfaf83a7d13aa9e03a6f61ef6c07c6a1e
JOY <zandy212@126.com>
2018-03-27 22:05:49 +0800

    Initial commit

--- Flask/07-PFS/pfs.py
+++ Flask/07-PFS/pfs.py
@@ -1,10 +1,13 @@
-from flask import Flask
+# pfs : problem feedback system
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('base.html')
 
 if __name__ == '__main__':
     app.run()
